edb65133ed6e8b050df265247146d613cb7f904f
Parth Pant <parthpant4@gmail.com>
2022-10-15 12:22:40 +0530

    Implement valency 4 pipelined Adder

--- src/main/scala/Adder.scala
+++ src/main/scala/Adder.scala
@@ -126,3 +126,62 @@ class FastAdder(val width: Int, val valency: Int) extends Module {
     io.Sum := VecInit(ss).asUInt
     io.Cout := cs.last
 }
+
+class FastAdderPipelined(val width: Int) extends Module {
+    val io = IO(new Bundle {
+        val a = Input(UInt(width.W))
+        val b = Input(UInt(width.W))
+        val cin = Input(UInt(1.W))
+
+        val Sum = Output(UInt(width.W))
+        val Cout = Output(UInt(1.W))
+    })
+
+    val gpts_0 = GPTGen(width)(io.a, io.b)
+
+    val groups = VecInit(gpts_0.grouped(4).toVector.map{
+        case group => VecInit(ParallelPrefixTree(group) {
+            case (gpti, gptj) => GPTInit(gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t)
+        })
+    })
+
+    def reduce(gpts: Vec[Vec[GPT]], cin: Bool) : (Vec[Vec[GPT]], Bool) = {
+        def nextLayer(prev: Vec[Vec[GPT]], cin: Bool, offset: Int) : (Vec[Vec[GPT]], Bool) = {
+            if (offset > prev.size) {
+                (RegNext(prev), RegNext(cin))
+            } else {
+                val layer = VecInit(Vector.tabulate(prev.size) { i =>
+                    if (i < offset) {
+                        prev(i)
+                    } else {
+                        val (grpi, grpj) = (prev(i), prev(i - offset))
+                        val gptj = grpj.last
+                        VecInit(grpi.map{ case gpti => GPTInit (gpti.g | (gpti.p & gptj.g), gpti.p & gptj.p, gpti.t) })
+                    }
+                })
+
+                val reg = RegNext(layer)
+                val cinreg = RegNext(cin)
+                nextLayer(reg, cinreg, offset<<1)
+            }
+        }
+
+        nextLayer(gpts, cin, 1)
+    }
+
+    val (grps_last, cin) = reduce(groups, io.cin.asBool)
+    val gpts_last = grps_last.flatten
+
+    val gs = gpts_last.map(_.g)
+    val ps = gpts_last.map(_.p)
+    val ts = gpts_last.map(_.t)
+
+    val cs = cin +: gs.zip(ps).map{ case (gi, pi) => gi | (pi & cin) }
+
+    val ss = for (i <- 0 until width) yield {
+        cs(i) ^ ts(i)
+    }
+
+    io.Sum := RegNext(VecInit(ss).asUInt)
+    io.Cout := RegNext(cs.last)
+}
--- src/test/scala/AdderSpec.scala
+++ src/test/scala/AdderSpec.scala
@@ -2,24 +2,29 @@ package fpu
 
 import chisel3._
 import chiseltest._
-import scala.math.pow
+import scala.math.{pow, log}
+import scala.collection.mutable.ListBuffer
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import chiseltest.simulator.WriteVcdAnnotation
 
 class FastAdderSpec extends AnyFlatSpec with ChiselScalatestTester {
     behavior of "FastAdder"
 
     val r = scala.util.Random
-    for (w <- List(8, 16, 24, 28, 32, 64)) {
+    for (w <- List(8, 16, 32, 64)) {
         val hi = BigInt(2).pow(w)
         for (v <- List(1, 2, 4)) {
             it should s"Add $w bit numbers with valency $v" in {
                 test (new FastAdder(w, v)) { c =>
-                    for (i <- 1 to 500) {
+                    for (i <- 1 to 50) {
                         val a = BigInt(w, r)
                         val b = BigInt(w, r)
                         val cin = r.nextInt(2)
                         val cout = if (a + b + cin >=  hi) 1 else 0
                         val sum = (a+b+cin) & (hi-1) 
+
+                        // println(s"$a + $b + $cin = $sum, Cout = $cout")
 
                         c.io.a.poke(a.U)
                         c.io.b.poke(b.U)
@@ -29,6 +34,76 @@ class FastAdderSpec extends AnyFlatSpec with ChiselScalatestTester {
                         c.io.Cout.expect(cout.U)
                     }
                 }
+                // println("-------------------------------------------")
+            }
+        }
+    }
+}
+
+class FastAdderPipelinedSpec extends AnyFlatSpec with ChiselScalatestTester {
+    behavior of "FastAdderPipelined"
+
+    val lnOf2 = scala.math.log(2) // natural log of 2
+    def log2(x: Double): Double = scala.math.log(x) / lnOf2
+    val r = scala.util.Random
+
+    for (w <- List(8, 16, 32, 64)) {
+        val hi = BigInt(2).pow(w)
+        it should s"Add $w bit numbers" in {
+            test (new FastAdderPipelined(w)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+                val a = BigInt(w, r)
+                val b = BigInt(w, r)
+                val cin = r.nextInt(2)
+                val cout = if (a + b + cin >=  hi) 1 else 0
+                val sum = (a+b+cin) & (hi-1) 
+                val steps = 3 + log2(w/4).toInt
+
+                c.io.a.poke(a.U)
+                c.io.b.poke(b.U)
+                c.io.cin.poke(cin.U)
+
+                c.clock.step(steps)
+
+                c.io.Sum.expect(sum.U)
+                c.io.Cout.expect(cout.U)
+            }
+        }
+    }
+
+    for (w <- List(8, 16, 32, 64)) {
+        val n = 20 
+        it should s"Add $n pipelined $w-bit numbers" in {
+            test (new FastAdderPipelined(w)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+                val hi = BigInt(2).pow(w)
+                // 1 extra stage for group condense and another for carry-in sum
+                val steps = 3 + log2(w/4).toInt
+                val ips = for (i <- 1 to n) yield { 
+                    val a = BigInt(w, r)
+                    val b = BigInt(w, r)
+                    val cin = r.nextInt(2)
+                    val sum = (a+b+cin) & (hi-1) 
+                    val cout = if (a + b + cin >=  hi) 1 else 0
+                    (a, b, cin, sum, cout)
+                }
+
+                val outBuffer = new ListBuffer[(BigInt, BigInt)]()
+                for (ip <- ips) {
+                    c.io.a.poke(ip._1.U)
+                    c.io.b.poke(ip._2.U)
+                    c.io.cin.poke(ip._3.U)
+                    val o = (c.io.Sum.peek().litValue, c.io.Cout.peek().litValue)
+                    outBuffer += o
+                    c.clock.step(1)
+                }
+
+                for (i <- 1 to steps) {
+                    val o = (c.io.Sum.peek().litValue, c.io.Cout.peek().litValue)
+                    outBuffer += o
+                    c.clock.step(1)
+                }
+
+                val outList = outBuffer.toList.drop(outBuffer.size - n)
+                ips.zip(outList).map{ case (ip, op) => (ip._4, ip._5) shouldBe (op._1, op._2) }
             }
         }
     }
