b3c40349259b37826d4ad6114a9e3f520ac0a976
Parth Pant <parthpant4@gmail.com>
2022-10-15 22:41:31 +0530

    Improve Unpack32 and remove Unpack64

--- src/main/scala/Unpack.scala
+++ src/main/scala/Unpack.scala
@@ -1,51 +1,19 @@
 package fpu
 
 import chisel3._
+import chisel3.util._
 
 class Unpack32 extends Module {
     val io = IO(new Bundle {
         val in = Input(UInt(32.W))
         val s = Output(UInt(1.W))
         val e = Output(UInt(8.W))
-        val f = Output(UInt(23.W))
+        val f = Output(UInt(24.W))
     })
 
-    io.s := io.in(31)
+    val ins = io.in.asBools
 
-    val exp = Wire(Vec(8, UInt(1.W)))
-    val significand = Wire(Vec(23, UInt(1.W)))
-
-    for (i <- 0 until 8) {
-       exp(i) :=  io.in(23+i)
-    }
-    for (i <- 0 until 23) {
-       significand(i) := io.in(i)
-    }
-
-    io.e := exp.asUInt
-    io.f := significand.asUInt
-}
-
-class Unpack64 extends Module {
-    val io = IO(new Bundle {
-        val in = Input(UInt(64.W))
-        val s = Output(UInt(1.W))
-        val e = Output(UInt(11.W))
-        val f = Output(UInt(52.W))
-    })
-
-    io.s := io.in(63)
-
-    val exp = Wire(Vec(11, UInt(1.W)))
-    val significand = Wire(Vec(52, UInt(1.W)))
-
-    for (i <- 0 until 11) {
-       exp(i) :=  io.in(52+i)
-    }
-    for (i <- 0 until 52) {
-       significand(i) := io.in(i)
-    }
-
-    io.e := exp.asUInt
-    io.f := significand.asUInt
+    io.s := ins(31)
+    io.e :=  VecInit(ins.slice(23, 32)).asUInt
+    io.f := Cat(1.U, VecInit(ins.slice(0, 23)).asUInt)
 }
--- src/test/scala/UnpackSpec.scala
+++ src/test/scala/UnpackSpec.scala
@@ -14,22 +14,7 @@ class Unpack32Spec extends AnyFlatSpec with ChiselScalatestTester {
             c.io.in.poke(input)
             c.io.s.expect(1.U)
             c.io.e.expect("b10101100".U)
-            c.io.f.expect("b11110000111100001010101".U)
-        }
-    }
-}
-
-class Unpack64Spec extends AnyFlatSpec with ChiselScalatestTester {
-    behavior of "Unpack64"
-
-    it should "Unpack 64 bits" in {
-        test (new Unpack64) { c => 
-            val input = "b1101111010101101101111101110111101101001011010010000000100000001".U
-
-            c.io.in.poke(input)
-            c.io.s.expect(1.U)
-            c.io.e.expect("b10111101010".U)
-            c.io.f.expect("b1101101111101110111101101001011010010000000100000001".U)
+            c.io.f.expect("b111110000111100001010101".U)
         }
     }
 }
