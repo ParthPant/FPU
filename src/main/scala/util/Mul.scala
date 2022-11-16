504d5a6747f2df51dd851b0158405231cb3292ad
Parth Pant <parthpant4@gmail.com>
2022-11-16 16:28:58 +0530

    pipeline multiplier

--- src/main/scala/util/Mul.scala
+++ src/main/scala/util/Mul.scala
@@ -5,7 +5,7 @@ import chisel3.util._
 
 // Refer: https://www.giscafe.com/book/ASIC/CH02/CH02.16.php
 
-class ArrayMultiplier(val width : Int) extends Module {
+class ArrayMultiplier(val width : Int, val stages: Seq[Int]) extends Module {
     val io = IO(new Bundle {
         val a = Input(UInt(width.W))
         val b = Input(UInt(width.W))
@@ -13,32 +13,38 @@ class ArrayMultiplier(val width : Int) extends Module {
         val P = Output(UInt((2*width).W))
     })
 
-    val as = io.a.asBools
-    val bs = io.b.asBools
-
-    val pl = Wire(Vec(width, Bool()))
-    val ph = Wire(Vec(width, Bool()))
-
-    def makeTree(a: UInt, b: UInt) : (UInt, UInt) = {
-        def nextLayer(prev: (UInt, UInt), depth: Int) : (UInt, UInt) = {
+    def makeTree(a: UInt, b: UInt) : UInt = {
+        def nextLayer(ops: (UInt, UInt), prev: (UInt, UInt), res: UInt, depth: Int) : UInt = {
             if (depth < width) {
-                val next = Seq.tabulate(width)(n => FA(if (n < width-1) prev._1(n+1) else 0.B, a(n) & b(depth), prev._2(n)))
+                val (a, b) = ops
+                val next = Seq.tabulate(width)(n => FA(if (n < width-1) prev._1(n+1) else 0.B, a(n) & b(0), prev._2(n)))
+                val nextb = if (depth < width-1) b(width-depth-1, 1) else 0.U
                 val s = VecInit(next.map(_._1)).asUInt
                 val c = VecInit(next.map(_._2)).asUInt
-                pl(depth) := s(0)
-                nextLayer((s, c), depth+1)
+                val nextres = VecInit.tabulate(depth+1)(n => {
+                    if (n == depth) {
+                        s(0) 
+                    } else {
+                        res(n)
+                    }
+                }).asUInt
+
+                if (stages.contains(depth)) {
+                    nextLayer((RegNext(a), RegNext(nextb)), (RegNext(s), RegNext(c)), RegNext(nextres), depth+1)
+                } else {
+                    nextLayer((a, nextb), (s, c), nextres, depth+1)
+                }
             } else {
                 val x = Cat(0.B, prev._1(width-1, 1))
                 val y = prev._2
                 val (s, c) = RippleCarryAdder(width)(x, y, 0.U) 
-                ph := s.asBools
-                (s, c) 
+                val product = Cat(s, res)
+                product
             }
         }
-        nextLayer((0.U,0.U), 0)
+
+        nextLayer((a, b), (0.U,0.U), 0.U, 0)
     }
 
-    makeTree(io.a, io.b)
-
-    io.P := Cat(ph.asUInt, pl.asUInt)
+    io.P := RegNext(makeTree(io.a, io.b))
 }
--- src/test/scala/util/MulSpec.scala
+++ src/test/scala/util/MulSpec.scala
@@ -3,24 +3,46 @@ package fpu
 import chisel3._
 import chiseltest._
 import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
+import chiseltest.simulator.WriteVcdAnnotation
+import scala.collection.mutable.ListBuffer
 
 class ArrayMultiplierSpec extends AnyFlatSpec with ChiselScalatestTester {
     behavior of "ArrayMultiplier"
 
     val r = scala.util.Random
+    val n = 5
 
-    for (w <- List(8, 16, 32, 64)) {
+    for (w <- List(8, 16, 24, 32)) {
+        val stages = List.range(0, w-1) //List(w/3, 2*w/3)
+        val steps = stages.size + 1
         val hi = BigInt(2).pow(w)
-        it should s"Multiply $w bit numbers" in {
-            test (new ArrayMultiplier(w)) { c =>
-                val a = BigInt(w, r)
-                val b = BigInt(w, r)
-                val prod = a * b
+        it should s"Multiply $n $w-bit numbers in $steps cycles" in {
+            test (new ArrayMultiplier(w, stages)).withAnnotations(Seq(WriteVcdAnnotation)) { c =>
+                val ips = for (i <- 1 to n) yield { 
+                    val a = BigInt(w, r)
+                    val b = BigInt(w, r)
+                    val prod = a * b
+                    (a, b, prod)
+                }
 
-                c.io.a.poke(a.U)
-                c.io.b.poke(b.U)
+                val outBuffer = new ListBuffer[BigInt]()
+                for (ip <- ips) {
+                    c.io.a.poke(ip._1.U)
+                    c.io.b.poke(ip._2.U)
+                    val o = c.io.P.peek().litValue
+                    outBuffer += o
+                    c.clock.step(1)
+                }
 
-                c.io.P.expect(prod)
+                for (i <- 1 to steps) {
+                    val o = c.io.P.peek().litValue
+                    outBuffer += o
+                    c.clock.step(1)
+                }
+
+                val outList = outBuffer.toList.drop(outBuffer.size - n)
+                ips.zip(outList).map{ case (ip, op) => ip._3 shouldBe op }
             }
         }
     }
