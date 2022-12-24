bd2db841f8560936e2a4feff8aaaffbfc82cd830
Parth Pant <parthpant4@gmail.com>
2022-12-24 22:39:42 +0530

    better Delay code

--- src/main/scala/util/Delay.scala
+++ src/main/scala/util/Delay.scala
@@ -4,12 +4,7 @@ import chisel3._
 
 object Delay {
     def apply[T <: Data](data: T, t: Int): T = {
-        def makereg(data: T, n: Int): T = {
-            if (n < t)
-                makereg(RegNext(data), n+1)
-            else 
-                data
-        }
-        makereg(data, 0)
+        assert(t > 0)
+        (0 until t).foldLeft(data){(prev, _) => RegNext(prev)}
     }
 }
